830e493c0c5ff0f894d8e4f664f34cb5556fbea9
mallikaahuja <mallikaahuja98@gmail.com>
2025-07-04 10:42:48 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get install -y graphviz libgraphviz-dev
-python -m pip install --upgrade pip
-pip install -r requirements.txt
+pip install --force-reinstall -r requirements.txt
